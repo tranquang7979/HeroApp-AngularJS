2fad69d0d47089a00c9ec928d7489cee04a2e350
Steven Tran <tranquang7979@hotmail.com>
2019-08-08 17:55:31 +0700

    finish checking the Hero app.

--- src/app/hero-detail/hero-detail.component.ts
+++ src/app/hero-detail/hero-detail.component.ts
@@ -29,4 +29,9 @@ export class HeroDetailComponent implements OnInit {
       .subscribe(hero => this.hero = hero);
   }
 
+  save(): void {
+    this.heroService.updateHero(this.hero)
+      .subscribe(() => this.goBack());
+  }
+  
 }
